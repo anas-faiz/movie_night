ae33d17b05a15f7e6a84e416f2887c4ece1cc458
Anas-Faiz <anasfaiz0811@gmail.com>
2026-01-29 19:27:35 -0800

    tmdb api call made

--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Header from "../components/Header";
+import { options } from "../utils/constants";
 
 function BrowsePage(){
+
+    const getNowPlayingMovies = async ()=>{
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', options)
+        const json = await data.json();
+        console.log(json.results)
+    }
+
+    useEffect(()=>{
+        getNowPlayingMovies();
+    },[])
+
     return(
         <div className="w-full bg-amber-900">
             <Header/>
