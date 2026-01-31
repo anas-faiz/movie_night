1e85f92bc29c81785ea1ee4e6a4d94390215594e
Anas-Faiz <anasfaiz0811@gmail.com>
2026-01-30 18:20:42 -0800

    useNowPlayingMovies

--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,23 +1,9 @@
-import { useEffect } from "react";
 import Header from "../components/Header";
-import { options } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addNowPlayingMovies } from "../utils/movieSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 function BrowsePage(){
 
-    const dispatch = useDispatch();
-
-    const getNowPlayingMovies = async ()=>{
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', options)
-        const json = await data.json();
-        console.log(json.results)
-        dispatch(addNowPlayingMovies(json.results))
-    }
-
-    useEffect(()=>{
-        getNowPlayingMovies();
-    },[])
+    useNowPlayingMovies();
 
     return(
         <div className="w-full bg-amber-900">
