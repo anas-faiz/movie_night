ebc5eb98a6b96c718258459f6e993570e43ed3ad
Anas-Faiz <anasfaiz0811@gmail.com>
2026-01-31 16:31:47 -0800

    youtube embed added

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,21 +1,40 @@
-import React, { useEffect } from 'react'
-import { options } from '../utils/constants'
+import React, { useEffect } from "react";
+import { options } from "../utils/constants";
 
-const VideoBackground = ({id}) => {
-
-  const getMovieVideo = async()=>{
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+const VideoBackground = ({ id }) => {
+  const getMovieVideo = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options,
+    );
     const json = await data.json();
-    console.log("Video data",json)
-  }
+    console.log("Video data", json);
 
-  useEffect(()=>{
+    const filterData = json.results.filter((video) => video.type == "Trailer");
+
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+
+    console.log("trailer", trailer);
+  };
+
+  useEffect(() => {
     getMovieVideo();
-  },[])
+  }, []);
 
   return (
-    <div>VideoBackground</div>
-  )
-}
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
