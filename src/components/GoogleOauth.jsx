957792adec0d189a05137a6a4cb0609d8afabc87
Anas-Faiz <anasfaiz0811@gmail.com>
2026-02-04 12:23:51 -0800

    loader added

--- src/components/GoogleOauth.jsx
+++ src/components/GoogleOauth.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import google from "../assets/google.svg";
 
 const GoogleOauth = () => {
+  const [isloading, setIsLoading] = useState(false);
+
+  const handleSignIn = () => {
+    setIsLoading(true);
+
+    try {
+    } catch (error) {}
+  };
+
   return (
     <div className="w-full flex justify-center mt-3">
       <button
         type="button"
+        onClick={handleSignIn}
         className="
           flex items-center gap-3
           bg-white text-black
@@ -20,12 +31,21 @@ const GoogleOauth = () => {
           focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
         "
       >
-        {/* Google Icon */}
-        <div className="w-5 h-5 flex items-center justify-center font-bold">
-          <img src={google} alt="Google" className="w-5 h-5" />
-        </div>
-
-        <span className="text-sm">Continue with Google</span>
+        {isloading ? (
+          <>
+            {/* Spinner */}
+            <span className="w-5 h-5 border-2 border-gray-400 border-t-transparent rounded-full animate-spin" />
+            <span className="text-sm">Signing in...</span>
+          </>
+        ) : (
+          <>
+            {/* Google Icon */}
+            <div className="w-5 h-5 flex items-center justify-center font-bold">
+              <img src={google} alt="Google" className="w-5 h-5" />
+            </div>
+            <span className="text-sm">Continue with Google</span>
+          </>
+        )}
       </button>
     </div>
   );
